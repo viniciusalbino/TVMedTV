4904a5c69539549c9a3223de458b75c1d109ca3a
Vinicius Albino <viniciusmouralbino@gmail.com>
2017-02-23 22:55:50 -0300

    collection view animating cell

--- TVMed/HomeCatalogCollectionViewCell.swift
+++ TVMed/HomeCatalogCollectionViewCell.swift
@@ -19,20 +19,41 @@ class HomeCatalogCollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // These properties are also exposed in Interface Builder.
-        image.adjustsImageWhenAncestorFocused = true
         image.clipsToBounds = false
         
         self.titleLabel.tag = 10
         self.descrLabel.tag = 11
+        
+        serialNumberLabel.layer.cornerRadius = 2
+        serialNumberLabel.layer.masksToBounds = true
+        
+        layer.shadowColor = UIColor.lightGray.cgColor
+        layer.shadowOpacity = 1
+        layer.shadowOffset = CGSize(width: 0, height: -2)
     }
     
     func fill(item: Release) {
         self.titleLabel.text = item.titulo
         self.descrLabel.text = item.descricao
-        self.serialNumberLabel.text = item.numero
+        self.serialNumberLabel.text = "\(item.congresso)"
         if let url = URL(string: item.image) {
             self.image.kf.setImage(with:url)
+        }
+    }
+    
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        if context.nextFocusedView == self {
+            coordinator.addCoordinatedAnimations({
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.transform = CGAffineTransform(scaleX: 1.15, y: 1.15)
+                })
+            }, completion: nil)
+        } else if context.previouslyFocusedView == self {
+            coordinator.addCoordinatedAnimations({
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                })
+            }, completion: nil)
         }
     }
 }
